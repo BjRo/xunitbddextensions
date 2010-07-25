a4c639907aaf0973018b6043efc873ff63bacdc7
Bjoern Rochel <bjoern@bjro.de>
2010-07-25 17:11:57 +0200

    Added spec from cdeger which demonstrates a defect in the automocking container

---
+++ Source/xUnit.BDDExtensions.Specs/Automocking_a_class_with_a_ctor_dependency_on_an_ienumerable_of_an_arbitrary_interface_type.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit.Internal;
+
+namespace Xunit.Specs
+{
+    [Concern(typeof(AutoMockingContainer<>))]
+    public class When_trying_to_build_a_class_under_test_that_has_a_ctor_dependency_on_a_generic_collections_of_interface_types
+        : StaticContextSpecification
+    {
+        private AutoMockingContainer<TestClassWithGenericCollectionOfInterfaceTypes> _autoMocker;
+        private TestClassWithGenericCollectionOfInterfaceTypes _instance;
+
+        protected override void EstablishContext()
+        {
+            _autoMocker = new AutoMockingContainer<TestClassWithGenericCollectionOfInterfaceTypes>();
+        }
+
+        protected override void Because()
+        {
+            _instance = _autoMocker.ClassUnderTest;
+        }
+
+        [Observation]
+        public void Should_be_able_to_resolve_the_class_under_test()
+        {
+            _instance.ShouldNotBeNull();
+        }
+
+        [Observation]
+        public void Should_be_able_to_resolve_the_dependency()
+        {
+            _instance.Dependencies.ShouldNotBeNull();   
+        }
+
+        [Observation]
+        public void Should_inject_3_interface_stubs_into_the_collection()
+        {
+            _instance.Dependencies.Count().ShouldBeEqualTo(3);
+        }
+    }
+
+    public class TestClassWithGenericCollectionOfInterfaceTypes
+    {
+        public IEnumerable<ITestDependency> Dependencies;
+
+        public TestClassWithGenericCollectionOfInterfaceTypes(IEnumerable<ITestDependency> dependencies)
+        {
+            Dependencies = dependencies;
+        }
+    }
+
+    public interface ITestDependency
+    {
+    }
+}
--- Source/xUnit.BDDExtensions/Internal/AutoMockingContainer.cs
+++ Source/xUnit.BDDExtensions/Internal/AutoMockingContainer.cs
@@ -13,6 +13,7 @@
 // limitations under the License.
 //  
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using StructureMap.AutoMocking;
 
